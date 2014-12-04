b7bada7142d2fb4e2cfa97546051198ee99b83d7
sebseb7 <github@seb.exse.net>
2014-12-04 14:39:23 +0100

    reorder

--- libs/menu/menu_setup.c
+++ libs/menu/menu_setup.c
@@ -29,14 +29,14 @@ void menu_setup()
 		draw_text_16pt((LCD_WIDTH-text_width)>>1,9, "SETUP", 200,200,255);
 
 
-		draw_button_icon(11,45,92,1,"Zones",155,0,0,0,255,0);
-		draw_button_icon(114,45,92,1,"Actions",155,0,0,0,255,0);
+		draw_button_icon(11,45,92,1,"Devices",155,0,0,0,255,0);
+		draw_button_icon(114,45,92,1,"Scenes",155,0,0,0,255,0);
 		draw_button_icon(217,45,92,1,"Loops",155,0,0,0,255,0);
-		draw_button_icon(11,110,92,1,"Scenes",155,0,0,0,255,0);
-		draw_button_icon(114,110,92,1,"Devices",155,0,0,0,255,0);
-		draw_button_icon(217,110,92,1,"",55,55,55,0,255,0);
-		draw_button_icon(11,175,92,1,"Map CTRL",155,0,0,0,255,0);
-		draw_button_icon(114,175,92,1,"",55,55,55,0,255,0);
+		draw_button_icon(11,110,92,1,"Playback",155,0,0,0,255,0);
+		draw_button_icon(114,110,92,1,"Action",155,0,0,0,255,0);
+		draw_button_icon(217,110,92,1,"Mapping",155,0,0,0,255,0);
+		draw_button_icon(11,175,92,1,"",55,55,55,0,255,0);
+		draw_button_icon(114,175,92,1,"Save/Load",155,0,0,0,255,0);
 		draw_button_icon(217,175,92,1,"General",155,0,0,0,255,0);
 	}
 
@@ -74,7 +74,7 @@ void menu_setup()
 			}
 		}
 		
-		if(field == 5)
+		if(field == 1)
 		{
 			redraw=1;
 			set_current_execution(menu_setup_devices);

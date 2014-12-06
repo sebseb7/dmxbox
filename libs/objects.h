5301088dd0ca748f496fac2c9bdd83ab1058f208
sebseb7 <github@seb.exse.net>
2014-12-06 17:22:01 +0100

    object model update

--- libs/objects.h
+++ libs/objects.h
@@ -4,14 +4,23 @@
 
 // contains all devices like led-par's , fog-machines , ...
 
+
 struct {
 
-	uint16_t device_id;
+	uint8_t name_length;
+	char* name;
+	uint8_t channels;
+	uint8_t *channel_defaults; 
+
+
+} device_class_t
+
+struct {
+
+	device_class_t *device_class;
 	uint16_t base_address;
 	uint8_t name_length;
 	char *name;
-	uint8_t channels;
-	uint8_t *channel_defaults; 
 	uint16_t *channel_defaults_blackout; // will be called when blackout is issued , 0xffff means , do not overwrite
 	uint16_t *channel_defaults_identify; // will be called when identify is issued , 0xffff means , do not overwrite
 	uint16_t *channel_defaults_fullbright; // will be called when fullbright is issued , 0xffff means , do not overwrite
@@ -28,11 +37,10 @@ struct {
 
 struct {
 
-	uint16_t scene_id;
 	uint8_t name_length;
 	char* name;
 	uint8_t sets_length;
-	uint16_t *set_device;
+	devices_t **set_device;
 	uint8_t *set_channel;
 	uint8_t *set_value;
 
@@ -47,11 +55,10 @@ struct {
 
 struct {
 
-	uint16_t loop_id;
 	uint8_t name_length;
 	char* name;
 	uint16_t scene_length;
-	uint16_t *scene_ids;
+	scene_t **scenes;
 	uint16_t *hold_time;
 	uint8_t *morph_to_next; // morph between scenes
 	uint16_t repeats;
@@ -67,11 +74,10 @@ struct {
 
 struct {
 
-	uint16_t playback_id;
 	uint8_t name_length;
 	char* name;
 	uint16_t playback_length;
-	uint16_t *playback_ids;
+	loop_t **loops;
 
 } playback_t;
 
@@ -87,9 +93,9 @@ struct {
 	uint16_t action_id;
 	uint8_t name_length;
 	char* name;
-	uint16_t scene_ids;
+	scene_t **scenes;
 	uint8_t scene_priority;
-	uint16_t loop_ids;
+	loop_t **loops;
 	uint8_t loop_priority;
 	uint8_t type; // continuous, specific time/length  or single action
 	uint16_t duration; //if type is 'length'
@@ -108,7 +114,7 @@ struct {
 	uint16_t mapping_id;
 	uint8_t name_length;
 	char* name;
-	uint16_t action_id;
+	action_t *action;
 	uint16_t input_id;
 
 } button_mapping_t

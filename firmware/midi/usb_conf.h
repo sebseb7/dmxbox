16400363872ffa079de012b24d15d6d343163b5b
sebseb7 <github@seb.exse.net>
2014-12-09 04:33:56 +0100

    warning

--- firmware/midi/usb_conf.h
+++ firmware/midi/usb_conf.h
@@ -154,6 +154,7 @@
 //#endif /* USB_OTG_HS_INTERNAL_DMA_ENABLED */
 
 /* __packed keyword used to decrease the data type alignment to 1-byte */
+#ifndef __packed
 #if defined (__CC_ARM)         /* ARM Compiler */
   #define __packed    __packed
 #elif defined (__ICCARM__)     /* IAR Compiler */
@@ -163,6 +164,7 @@
 #elif defined   (__TASKING__)  /* TASKING Compiler */
   #define __packed    __unaligned
 #endif /* __CC_ARM */
+#endif
 
 /**
   * @}
